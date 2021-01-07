837df60e0b39f8b318321d24d088e152bfcb6f42
safayet.safayet@labaidpharma.com <safayet@labaidpharma.com>
2021-01-08 00:45:49 +0600

    customerType

---
+++ src/app/Classes/ClientBusiness/CustomerTypeModel.ts
@@ -0,0 +1,11 @@
+export class CustomerTypeModel {
+
+	public ID : number=0;
+	public TypeCode : string;
+	public TypeName : string;
+	public isActive : boolean=true;
+	public CreatedBy : string;
+	public CreatedOn : string;
+	public UpdatedBy : string;
+	public UpdatedOn : string;
+}
--- src/app/ClientBusiness/Master/CutomerType/CutomerType.ts
+++ src/app/ClientBusiness/Master/CutomerType/CutomerType.ts
@@ -1,13 +1,43 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthGuard } from '../../../authGuard.guard';
+import { UserService } from '../../../Services/User.service';
+import { NotificationService } from "../../../Services/Notification.service";
+
+//Service  
+import { UserAccessService } from "../../../Services/UserAccess.service";
+import { Library } from 'src/app/library/library';
+import { ClientBusinessService } from '../../../Services/ClientBusiness.service';
+
+// classess
+import { CustomerTypeModel   } from '../../../Classes/ClientBusiness/CustomerTypeModel';
 
 @Component({
 	templateUrl: 'CutomerType.html'
 })
 export class CutomerType implements OnInit {
+	user: any;
 
-	constructor() { }
+	customerTypeList: any[] = [];
+	customerTypeObj: CustomerTypeModel = new CustomerTypeModel();
+	SearchCustomerTypeList: string = '';
+
+	constructor(private userService: UserService, private authGuard: AuthGuard,
+		private Notification: NotificationService,private clientBusinessService: ClientBusinessService) { }
+
 
 	ngOnInit() {
+		this.user = this.userService.getLoggedUser();
+		this.authGuard.hasUserThisMenuPrivilege(this.user);
+
+		this.Notification.LoadingWithMessage('Loading...');
+		//  ----------------------
+		this.Notification.LoadingRemove();
+	}
+	saveCustomerType(){
+
+	}
+	ResetModel(){
+		this.customerTypeObj = new CustomerTypeModel();
 	}
 
 }
