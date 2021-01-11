4ab556263198d5ad901be47e0c4530b6a5e14fa2
User <User@DESKTOP-QVEKPLI>
2021-01-12 00:31:15 +0600

    .

--- src/app/ClientBusiness/Master/_PackTour/_PackTour.ts
+++ src/app/ClientBusiness/Master/_PackTour/_PackTour.ts
@@ -20,7 +20,7 @@ export class _PackTour implements OnInit {
 	packageList: any = [];
 	packageDetailObj: PackageDetailsModel[] = [];
 	packageMasterObj: PackageMasterModel = new PackageMasterModel();
-	SearchPackageList:string ="";
+	SearchPackageList: string = "";
 	constructor(private userService: UserService, private authGuard: AuthGuard,
 		private Notification: NotificationService, private clientBusinessService: ClientBusinessService) { }
 
@@ -38,10 +38,10 @@ export class _PackTour implements OnInit {
 		this.Notification.LoadingWithMessage('Loading...');
 		this.clientBusinessService.getPackageList()
 			.subscribe(
-				data =>this.setPackageList(data),
+				data => this.setPackageList(data),
 				error => this.Notification.Error(error)
-			);		
-	}	
+			);
+	}
 	setPackageList(data) {
 		this.packageList = data;
 		console.log(this.packageList);
@@ -77,7 +77,7 @@ export class _PackTour implements OnInit {
 	savePackage() {
 		// console.log(this.packageMasterObj);
 		// console.log(this.packageDetailObj);
-
+		debugger
 		// Validation
 		if (!this.validateModel()) return;
 
@@ -95,24 +95,33 @@ export class _PackTour implements OnInit {
 	setaveResult(Data: any) {
 		if (Data.ID > 0) {
 			this.Notification.Success('Saved Successfully.');
+			this.ResetModel();
 			document.getElementById("PackageList_tab").click();
+			this.getPackageList(); 
 		}
 		else {
 			this.Notification.Failure('Unable to save data.');
 			console.log(Data)
 		}
 	}
+	ResetModel() {
+		this.packageMasterObj = new  PackageMasterModel();		
+		this.setNewPackageDetails();  
+	}
 	validateModel() {
 		debugger
+		var result = true;
 		try {
 			if (Library.isNullOrEmpty(this.packageMasterObj.PackageName)) {
 				this.Notification.Warning('Please package name.');
-				return false;
+				result = false;
+				return;
 			}
 
 			if (Library.isNuLLorUndefined(this.packageMasterObj.NoOfDay)) {
 				this.Notification.Warning('Please No Of Days.');
-				return false;
+				result = false;
+				return
 			}
 			var validDetails = 0;
 			this.packageDetailObj.forEach(item => {
@@ -121,13 +130,15 @@ export class _PackTour implements OnInit {
 
 					if (Library.isNullOrZero(item.EventPrice)) {
 						this.Notification.Warning('Please Select Event Price.');
-						return false;
+						result = false;
+						return;
 					}
 					validDetails += 1;
 				}
 				else {
 					this.Notification.Warning('Please Select Event Name.');
-					return false;
+					result = false;
+					return;
 				}
 			});
 		}
@@ -135,6 +146,7 @@ export class _PackTour implements OnInit {
 			this.Notification.Warning('Please Select item.');
 			return false;
 		}
+		return result;
 	}
-	
+
 }
