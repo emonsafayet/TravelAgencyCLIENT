1a7fefa5ab5cba9eccdfb525748fd664842a58f1
User <User@DESKTOP-QVEKPLI>
2021-03-08 22:53:45 +0600

    ,

--- src/app/ClientBusiness/Transaction/AdvancePaymentClearance/AdvancePaymentClearance.ts
+++ src/app/ClientBusiness/Transaction/AdvancePaymentClearance/AdvancePaymentClearance.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { AuthGuard } from '../../../authGuard.guard';
 import { UserService } from '../../../Services/User.service';
 import { NotificationService } from "../../../Services/Notification.service"; 
+import { Library } from 'src/app/library/library';
+import { Common } from "../../../library/common";
 //Service   
 import { TransactionCommonService } from '../../../Services/TransactionCommon.service'; 
 
 // classess
 import { AdvPaymentClearance } from '../../../Classes/Transaction/AdvPaymentClearance';
-
+declare var moment: any;
 @Component({
 	templateUrl: 'AdvancePaymentClearance.html'
 })
@@ -35,13 +37,15 @@ export class AdvancePaymentClearance implements OnInit {
 			);
 	}
 	setadvancePaymentClearanceList(data) {
+		for(var i of data)  i.ClearingDate =  moment().format(Common.SQLDateFormat);	 
+		
 		this.advancePaymentClearanceList = data;
 		this.Notification.LoadingRemove();
 	}
 	Update(obj) {
 		console.log(obj);
 		if(obj.isClear ==true)
-		{
+		{ 
 			this.transactionCommonService.updateadvanceClearance(obj)
 			.subscribe(
 				data => this.setupdateadvanceClearance(data),
