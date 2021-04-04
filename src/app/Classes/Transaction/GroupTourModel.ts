3000f099a11addd8e1021f7e54a97cd6f7209c78
User <User@DESKTOP-QVEKPLI>
2021-04-04 14:23:37 +0600

    Event Details UI Done

--- src/app/Classes/Transaction/GroupTourModel.ts
+++ src/app/Classes/Transaction/GroupTourModel.ts
@@ -1,45 +1,110 @@
-export class GroupTourMasterDTO
-{
-
-	public ID : number;
-	public TransactionCode : string;
-	public TransactionDate : string;
-	public TourName : string;
-	public TourStartDate : string;
-	public TourEndDate : string;
-	public NoofDays : number;
-	public NoofNight : number;
-	public NoofParticipent : number;
-	public SalesReferenceCode : string;
-	public IsFinalLocked : boolean;
-	public CreatedBy : string;
-	public CreatedOn : string;
-	public UpdatedBy : string;
-	public UpdatedOn : string;
-	public customers:string;
+export class GroupTourMasterDTO {
+	public ID: number = 0;
+	public TransactionCode: string;
+	public TransactionDate: string;
+	public TourName: string;
+	public TourStartDate: string;
+	public TourEndDate: string;
+	public NoofDays: number;
+	public NoofNight: number;
+	public NoofParticipent: number;
+	public SalesReferenceCode: string;
+	public IsFinalLocked: boolean;
+	public CreatedBy: string;
+	public CreatedOn: string;
+	public UpdatedBy: string;
+	public UpdatedOn: string;
+	public customers: string;
 }
-export class GroupTourCustomerDTO{
-	public ID : number;
-	public TransactionCode : string;
-	public CustomerCode : string="0";
-	public NetPayableAmt : number;
-	public CardCode : string;
-	public Currency : string;
-	public CurrencyRate : number;
-	public CreatedBy : string;
-	public CreatedOn : string;
-	public UpdatedBy : string;
-	public UpdatedOn : string;
+export class GroupTourCustomerDTO {
+	public ID: number;
+	public TransactionCode: string;
+	public CustomerCode: string = "0";
+	public NetPayableAmt: number;
+	public CardCode: string;
+	public Currency: string;
+	public CurrencyRate: number;
+	public CreatedBy: string;
+	public CreatedOn: string;
+	public UpdatedBy: string;
+	public UpdatedOn: string;
 }
 export class GroupTourMemberDTO {
-	public ID : number;
-	public TransactionCode : string;
-	public CustomerCode : string="0";
-	public NameofParticipent : string;
-	public PassportNo : string;
-	public IsChild : boolean;
-	public CreatedBy : string;
-	public CreatedOn : string;
-	public UpdatedBy : string;
-	public UpdatedOn : string;
+	public ID: number;
+	public TransactionCode: string;
+	public CustomerCode: string = "0";
+	public NameofParticipent: string;
+	public PassportNo: string;
+	public IsChild: boolean;
+	public CreatedBy: string;
+	public CreatedOn: string;
+	public UpdatedBy: string;
+	public UpdatedOn: string;
 }
+export class GroupTourVisaDTO {
+
+	public ID: number;
+	public TransactionCode: string;
+	public ServiceCode: string;
+	public CustomerCode: string="0";
+	public NameofParticipent: string;
+	public CountryCode: string="0";
+	public MainPrice: number;
+	public ServiceParcent: number;
+	public ServiceCharge: number;
+	public Discount: number;
+	public TotalPayableamt: number;
+
+}
+export class GroupTourAirticketDTO {
+	public ID: number;
+	public TransactionCode: string;
+	public ServiceCode: string;
+	public CustomerCode: string="0";
+	public NameofParticipent: string;
+	public ProviderCode:string="0";
+	public AirlinesCode: string="0";
+	public Route: string;
+	public MainPrice: number;
+	public GovtTax: number;
+	public Commision: number;
+	public ServiceParcent: number;
+	public ServiceCharge: number;
+	public Discount: number;
+	public IsCancel: boolean;
+	public CancelationCharge: number;
+	public TotalPayableamt: number;
+}
+export class GroupTourHotelBookingDTO {
+	public ID: number;
+	public TransactionCode: string;
+	public ServiceCode: string;
+	public CustomerCode: string="0";
+	public HotelName: string;
+	public ProviderCode: string="0";
+	public RoomType:  string="0";
+	public MainPrice: number;
+	public ServiceParcent: number;
+	public ServiceCharge: number;
+	public Discount: number;
+	public IsCancel: boolean;
+	public CancelationCharge: number;
+	public TotalPayableamt: number;
+
+}
+
+export class GroupTourTotalPackage {
+	public ID: number;
+	public TransactionCode: string;
+	public ServiceCode: string;
+	public CustomerCode: string="0";
+	public ProviderCode: string="0";
+	public MainPrice: number;
+	public ServiceParcent: number;
+	public ServiceCharge: number;
+	public Discount: number;
+	public IsCancel: boolean;
+	public CancelationCharge: number;
+	public TotalPayableamt: number;
+}
+
--- src/app/ClientBusiness/Transaction/GroupTour/GroupTour.ts
+++ src/app/ClientBusiness/Transaction/GroupTour/GroupTour.ts
@@ -10,8 +10,12 @@ import { ClientBusinessService } from '../../../Services/ClientBusiness.service'
 import { TransactionCommonService } from '../../../Services/TransactionCommon.service';
 import { Common } from "../../../library/common";
 //Classes
-import { GroupTourMasterDTO,GroupTourCustomerDTO,GroupTourMemberDTO } from '../../../Classes/Transaction/GroupTourModel';
+import {
+	GroupTourMasterDTO, GroupTourCustomerDTO, GroupTourMemberDTO,
+	GroupTourVisaDTO, GroupTourAirticketDTO, GroupTourHotelBookingDTO, GroupTourTotalPackage
+} from '../../../Classes/Transaction/GroupTourModel';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 
 declare var moment: any;
 @Component({
@@ -19,12 +23,23 @@ declare var moment: any;
 })
 export class GroupTour implements OnInit {
 	user: any;
+	countryList: any[] = [];
 	customerList: any[] = [];
+	travelProviderList: any[] = [];
+	airLineList: any[] = [];
+	filtercustomerList: any[] = [];
 	groupTourList: any[] = [];
+	roomTypeList: any[] = [];
+
 	groupTourMasterObj: GroupTourMasterDTO = new GroupTourMasterDTO();
 	groupTourCustomerObj: GroupTourCustomerDTO[] = [];
 	groupTourParticipentObj: GroupTourMemberDTO[] = [];
-	showEvent:boolean=false;
+	groupTourVisaObj: GroupTourVisaDTO[] = [];
+	groupTourAirticketObj: GroupTourAirticketDTO[] = [];
+	groupTourHotelBookingObj: GroupTourHotelBookingDTO[] = [];
+	groupTourTotalPackageObj: GroupTourTotalPackage[] = [];
+
+	showEvent: boolean = false;
 	salesStaffList: any[] = [];
 	grouptourinfoList: any[] = [];
 	constructor(private userService: UserService, private authGuard: AuthGuard,
@@ -42,34 +57,36 @@ export class GroupTour implements OnInit {
 		this.Notification.LoadingRemove();
 	}
 	setNewCustomerDetails() {
-		this.groupTourCustomerObj = []; 
+		this.groupTourCustomerObj = [];
 		this.addNewColumnForCustomerDetail();
 	}
 	addNewColumnForCustomerDetail() {
-		var details = new GroupTourCustomerDTO(); 
+		var details = new GroupTourCustomerDTO();
 		this.groupTourCustomerObj.push(details);
 	}
 	addCustomerDetailsNew(value, event) {
 		this.addNewColumnForCustomerDetail();
 		setTimeout(() => this.selectNext(value, event), 500)
-	} 
-	removeCustomerDetails(index: number){
+	}
+	removeCustomerDetails(index: number) {
 		this.groupTourCustomerObj.splice(index, 1);
+		this.filterCustomers();
 	}
 	//Participent
 	setNewParticipentDetails() {
-		this.groupTourParticipentObj = []; 
+		this.groupTourParticipentObj = [];
 		this.addNewColumnForParticipentDetail();
 	}
-	addParticipentDetailsNew(value, event){
+	addParticipentDetailsNew(value, event) {
+		this.filterCustomers();
 		this.addNewColumnForParticipentDetail();
 		setTimeout(() => this.selectNext(value, event), 500)
 	}
 	addNewColumnForParticipentDetail() {
-		var details = new GroupTourMemberDTO(); 
+		var details = new GroupTourMemberDTO();
 		this.groupTourParticipentObj.push(details);
 	}
-	removeparticipentDetails(index: number){
+	removeparticipentDetails(index: number) {
 		this.groupTourParticipentObj.splice(index, 1);
 	}
 	selectNext(key, e): void {
@@ -82,6 +99,8 @@ export class GroupTour implements OnInit {
 			}
 		}
 	}
+
+
 	GETSalesStaffLIST() {
 		this.Notification.LoadingWithMessage('Loading...');
 		this.transactionCommonService.GETSalesStaffLIST()
@@ -103,15 +122,164 @@ export class GroupTour implements OnInit {
 				error => this.Notification.Error(error)
 			);
 	}
+	//get travel Product List
+	getCountryList() {
+		this.Notification.LoadingWithMessage('Loading...');
+		this.clientBusinessService.getTravelcountryList()
+			.subscribe(
+				data => this.setCountryList(data),
+				error => this.Notification.Error(error)
+			);
+	}
+	setCountryList(data) {
+		this.countryList = data;
+		this.Notification.LoadingRemove();
+	}
 	setCustomerLIST(data) {
 		this.customerList = data;
 		this.Notification.LoadingRemove();
+	}
+
+	filterCustomers() {
+		const first = this.customerList;
+		const second = this.groupTourCustomerObj;
+		this.filtercustomerList = first.filter(x => second.map(y => y.CustomerCode).includes(x.CustomerCode));
 
 	}
 
 	// Tour Event Break Down
+	onCustomerChange(obj) {
+		this.filterCustomers();
+	}
+	//Start visa Details
+	setNewVisaDetails() {
+		this.groupTourVisaObj = [];
+		this.addNewColumnForVisaDetail();
+	}
+	addNewColumnForVisaDetail() {
+		var details = new GroupTourVisaDTO();
+		this.groupTourVisaObj.push(details);
+	}
+	addVisaDetailsNew(value, event) {
+		this.addNewColumnForVisaDetail();
+		setTimeout(() => this.selectNext(value, event), 500)
+	}
+	//End Visa End
 
-	addEventNew(){
-		this.showEvent=true;
+	//Start Airticket Details
+	setNewAirticketDetails() {
+		this.groupTourAirticketObj = [];
+		this.addNewColumnForAirticketDetail();
+	}
+	addNewColumnForAirticketDetail() {
+		var details = new GroupTourAirticketDTO();
+		this.groupTourAirticketObj.push(details);
+	}
+	addAirticketDetailsNew(value, event) {
+		this.addNewColumnForAirticketDetail();
+		setTimeout(() => this.selectNext(value, event), 500)
+	}
+	//End Airticket 
+
+	//Start Hotel Booking
+	setNewHotelBookingDetails() {
+		this.groupTourHotelBookingObj = [];
+		this.addNewColumnForHotelBookingDetail();
+	}
+	addNewColumnForHotelBookingDetail() {
+		var details = new GroupTourHotelBookingDTO();
+		this.groupTourHotelBookingObj.push(details);
+	}
+	addhotelBookingDetailsNew(value, event) {
+		this.addNewColumnForHotelBookingDetail();
+		setTimeout(() => this.selectNext(value, event), 500)
+	}
+	//End Hotel Booling
+
+	//Start Total Tour Package
+	setNewTourPackageDetails() {
+		this.groupTourTotalPackageObj = [];
+		this.addNewColumnForTourPakageDetail();
+	}
+	addNewColumnForTourPakageDetail() {
+		var details = new GroupTourTotalPackage();
+		this.groupTourTotalPackageObj.push(details);
+	}
+	addTourPackageDetailsNew(value, event) {
+		this.addNewColumnForTourPakageDetail();
+		setTimeout(() => this.selectNext(value, event), 500)
+	}
+	//End Total Tour Package
+
+	addEventNew() {
+		this.showEvent = true;
+		this.LoadDropDown();
+		this.filterCustomers();
+		this.showEventItemList();
+	}
+	showEventItemList() {
+		this.setNewVisaDetails();
+		this.setNewAirticketDetails();
+		this.setNewHotelBookingDetails();
+		this.setNewTourPackageDetails();
+	}
+	LoadDropDown() {
+		this.getCountryList();
+		this.GetTravelProviderList();
+		this.GetAirLineList();
+		this.getRoomTypeList();
+
+	}
+	//Start Event Details Drop Down
+	GetTravelProviderList() {
+		this.Notification.LoadingWithMessage('Loading...');
+		this.clientBusinessService.getProviderList()
+			.subscribe(
+				data => this.setTravelProviderList(data),
+				error => this.Notification.Error(error)
+			);
+	}
+	setTravelProviderList(data) {
+		this.travelProviderList = data;
+		this.Notification.LoadingRemove();
+
+	}
+	GetAirLineList() {
+		this.Notification.LoadingWithMessage('Loading...');
+		this.clientBusinessService.getAirlineList()
+			.subscribe(
+				data => this.setAirLineList(data),
+				error => this.Notification.Error(error)
+			);
+	}
+	setAirLineList(data) {
+		this.airLineList = data;
+		this.Notification.LoadingRemove();
+
+	}
+	getRoomTypeList() {
+		this.Notification.LoadingWithMessage('Loading...');
+		this.clientBusinessService.GETHotelRoomTypeLIST()
+			.subscribe(
+				data => this.setRoomTypeList(data),
+				(error) => this.showError(error)
+			);
+	}
+	setRoomTypeList(data) {
+		this.roomTypeList = data;
+		this.Notification.LoadingRemove();
+	}
+	//End Event Details Drop Down
+	showError(error) {
+		this.Notification.Error(error);
+		this.Notification.LoadingRemove();
+	}
+	isNumberKey(evt) {
+		var charCode = (evt.which) ? evt.which : evt.keyCode;
+		if (charCode != 46 && charCode > 31
+			&& (charCode < 48 || charCode > 57)) {
+			return false;
+		}
+		return true;
 	}
 }
